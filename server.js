9b4fda4e01af8e47a9ee382990e61d8cb2d8d72a
Sasha Prokhorenko <djminikin@gmail.com>
2020-09-25 10:10:58 +0200

    Update db

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+const https = require("https");
 const jsonServer = require('json-server')
-const http = require("http");
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const data = require('./db.json')
 
 const port = process.env.PORT || 4000;
+const router = jsonServer.router(data)
+const server = jsonServer.create()
 
-server.use(middlewares)
+server.use(function (_, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+server.use(jsonServer.defaults(['./public']))
 server.use(router)
-server.listen(port, () => {
-  console.log('JSON Server is running')
-})
+server.listen(port)
 
-setInterval(function () {
-  http.get('http://minikin-json-server.herokuapp.com');
-}, 300000);
+console.log('Server is running on:', 'http://localhost:' + port)
+
+// setInterval(function () {
+//   http.get('http://minikin-json-server.herokuapp.com');
+// }, 300000);
